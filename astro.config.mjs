5c7be4623739a72414d0a2e841cf5f2d97255b88
pavlovadb <pavlovadb@gmail.com>
2025-12-18 19:28:48 -0800

    Add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://daniela_pavlova.github.io',
+  site: 'https://burrow-io.github.io',
   base: '/',
   output: 'static',
   build: {
